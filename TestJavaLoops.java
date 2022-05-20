5132fbd93f7ffa3fea5984337a94974346b6c390
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-20 19:27:15 +0530

    Add files via upload

---
+++ TestJavaLoops.java
@@ -0,0 +1,32 @@
+package com.vstl.loops;
+
+public class TestJavaLoops {
+
+	public static void main(String[] args) {
+
+		JavaLoops objJavaLoops = new JavaLoops();
+		
+		//for loop :
+		objJavaLoops.findArrayDataUsingForLoop();
+		
+		objJavaLoops.findArrayDataForLoop();
+		
+		//while loop :
+		objJavaLoops.belgaumDistCollegesName();
+		
+		objJavaLoops.belgaumCollegesName();
+		
+		
+		// do while loop :
+	    objJavaLoops.numberOfschoolsInBelgaumDist();
+
+		objJavaLoops.collegeNamesInBelgaum();
+
+	}
+	
+}
+
+		
+	
+
+
